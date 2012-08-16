1909182da0341aaaa8b96e375ffff04148a8d0b3
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2012-08-16 12:04:13 -0400

    Add valid license header

--- lib/Ekino/Metric/Type/Timer.php
+++ lib/Ekino/Metric/Type/Timer.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace Ekino\Metric\Type;
+/*
+ * This file is part of the Ekino PHP metric project.
+ *
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace Ekino\Metric\Type;
 
 class Timer implements TimerInterface
 {
